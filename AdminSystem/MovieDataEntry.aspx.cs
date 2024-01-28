73c0677dc1c82fd7348a488975ce6b3967d33b39
P2674500 <P2674500@my365.dmu.ac.uk>
2024-01-28 20:10:05 +0000

    Added a 'Find' button to the movie data entry form.

--- AdminSystem/MovieDataEntry.aspx.cs
+++ AdminSystem/MovieDataEntry.aspx.cs
@@ -38,4 +38,28 @@ public partial class _1_DataEntry : System.Web.UI.Page
     {
 
     }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        clsMovie AMovie = new clsMovie();
+        Int32 MovieId;
+        Boolean Found = false;
+        MovieId = Convert.ToInt32(txtMovieId.Text);
+        Found = AMovie.Find(MovieId);
+        if (Found == true) 
+        {
+            //display in form
+            txtMovieId.Text = AMovie.MovieId.ToString();
+            txtTitle.Text = AMovie.Title;
+            txtRuntime.Text = AMovie.Runtime.ToString();
+            txtReleaseDate.Text = AMovie.ReleaseDate.ToString();
+            txtDescription.Text = AMovie.Description;
+            txtGenre.Text = AMovie.Genre;
+            txtRating.Text = AMovie.Rating.ToString();
+            txtDirectors.Text = AMovie.Directors;
+            txtWriters.Text = AMovie.Writers;
+            txtStarActors.Text = AMovie.StarActors;
+            txtCoverImage.Text = AMovie.CoverImage;
+        }
+    }
 }

7d3ae6aecd288f68f3b56be4999bcc68b52bf8a9
P2674500 <P2674500@my365.dmu.ac.uk>
2024-01-31 02:43:28 +0000

    Created collection class and collection test class

---
+++ Testing4/tstMovieCollection.cs
@@ -0,0 +1,31 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstMovieCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsMovieCollection AllMovies = new clsMovieCollection();
+            Assert.IsNotNull(AllMovies);
+        }
+
+        [TestMethod]
+        public void MovieListOK()
+        {
+            clsMovieCollection AllMovies = new clsMovieCollection();
+            //data needs to be a list of objects
+            List<clsMovie> TestList = new List<clsMovie>();
+            clsMovie TestItem = new clsMovie();
+
+            TestItem.MovieId = 1;
+            TestItem.Title = "Test title";
+        }
+
+    }
+}

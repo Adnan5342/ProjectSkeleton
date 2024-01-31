d5e32e7554cb874e5e0d9e173d9bb9e53d6cb2cd
P2674500 <P2674500@my365.dmu.ac.uk>
2024-01-31 04:57:21 +0000

    Did some collection and count testing for movies

--- ClassLibrary/clsMovieCollection.cs
+++ ClassLibrary/clsMovieCollection.cs
@@ -1,8 +1,67 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace ClassLibrary
 {
     public class clsMovieCollection
     {
+        //private data member
+        List<clsMovie> mMovieList = new List<clsMovie>();
+
+        public clsMovieCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblMovie_SelectAll");
+            RecordCount = DB.Count;
+
+            while (Index < RecordCount)
+            {
+                clsMovie AMovie = new clsMovie();
+
+                AMovie.MovieId = Convert.ToInt32(DB.DataTable.Rows[Index]["MovieId"]);
+                AMovie.Title = Convert.ToString(DB.DataTable.Rows[Index]["Title"]);
+                AMovie.Runtime = TimeSpan.Parse(Convert.ToString(DB.DataTable.Rows[Index]["Runtime"]));
+                AMovie.ReleaseDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["ReleaseDate"]);
+                AMovie.Description = Convert.ToString(DB.DataTable.Rows[Index]["Description"]);
+                AMovie.Genre = Convert.ToString(DB.DataTable.Rows[Index]["Genre"]);
+                AMovie.Rating = Convert.ToDouble(DB.DataTable.Rows[Index]["Rating"]);
+                AMovie.Directors = Convert.ToString(DB.DataTable.Rows[Index]["Directors"]);
+                AMovie.Writers = Convert.ToString(DB.DataTable.Rows[Index]["Writers"]);
+                AMovie.StarActors = Convert.ToString(DB.DataTable.Rows[Index]["StarActors"]);
+                AMovie.CoverImage = Convert.ToString(DB.DataTable.Rows[Index]["CoverImage"]);
+
+                mMovieList.Add(AMovie);
+                Index++;
+            }
+        }
+
+        public List<clsMovie> MovieList
+        {
+            get
+            {
+                return mMovieList;
+            }
+            set
+            {
+                mMovieList = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return mMovieList.Count;
+            }
+            set
+            {
+                //do later
+            }
+        }
+
+        public clsMovie ThisMovie { get; set; }
     }
 }
--- Testing4/tstMovieCollection.cs
+++ Testing4/tstMovieCollection.cs
@@ -25,6 +25,65 @@ namespace Testing4
 
             TestItem.MovieId = 1;
             TestItem.Title = "Test title";
+            TestItem.Runtime = TimeSpan.Parse("1:30");
+            TestItem.ReleaseDate = Convert.ToDateTime("07/01/2002");
+            TestItem.Description = "A very exciting movie!";
+            TestItem.Genre = "Action";
+            TestItem.Rating = 4.5;
+            TestItem.Directors = "John Smith";
+            TestItem.Writers = "Smith John";
+            TestItem.StarActors = "Tom Holland";
+            TestItem.CoverImage = "/MovieCoverImages/UnchartedMoviePoster.jpg";
+
+            TestList.Add(TestItem);
+            AllMovies.MovieList = TestList;
+            Assert.AreEqual(AllMovies.MovieList, TestList);
+        }
+
+        [TestMethod]
+        public void ThisMoviePropertyOK()
+        {
+            clsMovieCollection AllMovies = new clsMovieCollection();
+            clsMovie TestMovie = new clsMovie();
+
+            TestMovie.MovieId = 1;
+            TestMovie.Title = "Test title";
+            TestMovie.Runtime = TimeSpan.Parse("1:30");
+            TestMovie.ReleaseDate = Convert.ToDateTime("07/01/2002");
+            TestMovie.Description = "A very exciting movie!";
+            TestMovie.Genre = "Action";
+            TestMovie.Rating = 4.5;
+            TestMovie.Directors = "John Smith";
+            TestMovie.Writers = "Smith John";
+            TestMovie.StarActors = "Tom Holland";
+            TestMovie.CoverImage = "/MovieCoverImages/UnchartedMoviePoster.jpg";
+
+            AllMovies.ThisMovie = TestMovie;
+            Assert.AreEqual(AllMovies.ThisMovie, TestMovie);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsMovieCollection AllMovies = new clsMovieCollection();
+            List<clsMovie> TestList = new List<clsMovie>();
+            clsMovie TestItem = new clsMovie();
+
+            TestItem.MovieId = 1;
+            TestItem.Title = "Test title";
+            TestItem.Runtime = TimeSpan.Parse("1:30");
+            TestItem.ReleaseDate = Convert.ToDateTime("07/01/2002");
+            TestItem.Description = "A very exciting movie!";
+            TestItem.Genre = "Action";
+            TestItem.Rating = 4.5;
+            TestItem.Directors = "John Smith";
+            TestItem.Writers = "Smith John";
+            TestItem.StarActors = "Tom Holland";
+            TestItem.CoverImage = "/MovieCoverImages/UnchartedMoviePoster.jpg";
+
+            TestList.Add(TestItem);
+            AllMovies.MovieList = TestList;
+            Assert.AreEqual(AllMovies.Count, TestList.Count);
         }
 
     }

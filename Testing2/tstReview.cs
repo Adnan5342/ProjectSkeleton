4b1eb747bfbc4abecf419c8d610c36070133a506
P2674500 <P2674500@my365.dmu.ac.uk>
2024-02-08 20:07:16 +0000

    Deleted front end pages from the admin system and created a seperate system for front end.

---
+++ Testing2/tstReview.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstReview
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsReview AReview = new clsReview();
+            Assert.IsNotNull(AReview);
+        }
+    }
+}

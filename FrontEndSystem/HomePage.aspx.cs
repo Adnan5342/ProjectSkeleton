52dd51f7ae346d297158d190bf2b5af3704c80ce
P2674500 <P2674500@my365.dmu.ac.uk>
2024-04-16 00:51:22 +0100

    I am having a problem with redirecting to the movie viewer page after selecting a movie from the home page.

--- FrontEndSystem/HomePage.aspx.cs
+++ FrontEndSystem/HomePage.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Registration;
 using ClassLibrary;
 
 public partial class HomePage : System.Web.UI.Page
@@ -10,15 +11,25 @@ public partial class HomePage : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         DisplayMovies();
+        DisplayShows();
     }
 
     void DisplayMovies()
     {
         clsMovieCollection Movies = new clsMovieCollection();
-        lstMovies.DataSource = Movies.MovieList;
-        lstMovies.DataValueField = "MovieId";
-        lstMovies.DataTextField = "Title";
-        lstMovies.DataBind();
+        lstMovieList.DataSource = Movies.MovieList;
+        lstMovieList.DataValueField = "MovieId";
+        lstMovieList.DataTextField = "Title";
+        lstMovieList.DataBind();
+    }
+
+    void DisplayShows()
+    {
+        clsShowCollection Shows = new clsShowCollection();
+        lstShows.DataSource = Shows.ShowList;
+        lstShows.DataValueField = "ShowId";
+        lstShows.DataTextField = "Title";
+        lstShows.DataBind();
     }
 
     protected void imgBtnProfile_Click(object sender, ImageClickEventArgs e)
@@ -29,5 +40,37 @@ public partial class HomePage : System.Web.UI.Page
     protected void imgBtnLogo_Click(object sender, ImageClickEventArgs e)
     {
         Response.Redirect("HomePage.aspx");
+    }
+
+    protected void btnViewMovie_Click(object sender, EventArgs e)
+    {
+        Int32 MovieId;
+
+        if (lstMovieList.SelectedIndex == -1)
+        {
+            MovieId = Convert.ToInt32(lstMovieList.SelectedValue);
+            Session["MovieId"] = MovieId;
+            Response.Redirect("MovieViewer.aspx");
+        }
+        else 
+        {
+            lblMovieError.Text = "Please select a movie to view.";
+        }
+    }
+
+    protected void btnViewShow_Click(object sender, EventArgs e)
+    {
+        Int32 ShowId;
+
+        if (lstShows.SelectedIndex != -1)
+        {
+            ShowId = Convert.ToInt32(lstShows.SelectedValue);
+            Session["ShowId"] = ShowId;
+            Response.Redirect("ShowViewer.aspx");
+        }
+        else
+        {
+            lblShowError.Text = "Please select a show to view.";
+        }
     }
 }
---
+++ FrontEndSystem/MovieViewer.aspx.cs
@@ -0,0 +1,32 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MovieViewer : System.Web.UI.Page
+{
+    Int32 MovieId;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            MovieId = Convert.ToInt32(Session["MovieId"]);
+            if (MovieId != -1)
+            {
+                DisplayMovies();
+            }
+        }
+    }
+
+    void DisplayMovies()
+    {
+        clsMovieCollection Movies = new clsMovieCollection();
+        Movies.ThisMovie.Find(MovieId);
+
+        lblTitle.Text = Movies.ThisMovie.Title;
+    }
+}

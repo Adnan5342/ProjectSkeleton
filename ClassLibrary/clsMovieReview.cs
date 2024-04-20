1504cb7b7bac3b9050a8c836b2f0a1ea99d291fb
P2674500 <P2674500@my365.dmu.ac.uk>
2024-04-20 15:31:25 +0100

    .

---
+++ ClassLibrary/clsMovieReview.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsMovieReview
+    {
+        public clsMovieReview()
+        {
+        }
+
+        public int MovieReviewId { get; set; }
+        public int MovieId { get; set; }
+        public int MemberId { get; set; }
+        public double Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTime DatePosted { get; set; }
+    }
+}

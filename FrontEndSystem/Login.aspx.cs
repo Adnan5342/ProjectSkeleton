73896409ef107a37e5cc3f16f420d976c9033dac
P2674500 <P2674500@my365.dmu.ac.uk>
2024-04-20 17:54:16 +0100

    Removed Login from registration project and added to FrontEndSystem.

---
+++ FrontEndSystem/Login.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary;
+using Registration;
+
+public partial class Login :  System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+
+    protected void btnSignIn_Click(object sender, EventArgs e)
+    {
+        string username = txtUsername.Text.Trim();
+        string email = txtEmail.Text.Trim();
+        string password = txtPassword.Text.Trim();
+
+        using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"H:\\Project Database\\MovieMindsNetwork.mdf\";Integrated Security=True;Connect Timeout=30"))
+        {
+            connection.Open();
+
+            string query = "SELECT COUNT(*) FROM tblRegistration WHERE username=@username AND email=@email AND password=@password";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@username", username);
+            command.Parameters.AddWithValue("@email", email);
+            command.Parameters.AddWithValue("@password", password);
+
+            int v = (int)command.ExecuteScalar();
+
+            connection.Close();
+
+            if (v == 1)
+            {
+                Response.Redirect("HomePage.aspx");
+            }
+            else
+            {
+                lblError.Text = "Invalid username, email, or password.";
+                lblError.Visible = true;
+            }
+        }
+
+    }
+}

43bbd6c455a9bcd0dd421e094d6b676623e2acc5
P2674500 <P2674500@my365.dmu.ac.uk>
2024-04-22 06:37:28 +0100

    .

---
+++ ClassLibrary/clsMovieReviewCollection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsMovieReviewCollection
+    {
+        List<clsMovieReview> mMovieReviewList = new List<clsMovieReview>();
+
+        public List<clsMovieReview> MovieReviewList
+        {
+            get { return mMovieReviewList; }
+            set { mMovieReviewList = value; }
+        }
+
+
+        public int Count
+        {
+            get { return mMovieReviewList.Count; }
+            set { /*for later*/ }
+        }
+
+        public clsMovieReview ThisMovieReview { get; set; }
+
+        public clsMovieReviewCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblMovieReview_SelectAll");
+            RecordCount = DB.Count;
+
+            while (Index < RecordCount)
+            {
+                clsMovieReview AMovieReview = new clsMovieReview();
+
+                AMovieReview.MovieReviewId = Convert.ToInt32(DB.DataTable.Rows[Index]["MovieReviewId"]);
+                AMovieReview.MovieId = Convert.ToInt32(DB.DataTable.Rows[Index]["MovieId"]);
+                AMovieReview.MemberId = Convert.ToInt32(DB.DataTable.Rows[Index]["memberId"]);
+                //continueFromHere 
+            }
+        }
+
+    }
+}
